86ed1661eb7b32db5a28c3a42cc8227e83269cfe
Maria Zakharenkova <67202380+kusyash@users.noreply.github.com>
2021-02-01 15:29:38 +0300

    исправляет функции

--- js/main.js
+++ js/main.js
@@ -1,54 +1,37 @@
 // https://learn.javascript.ru/task/random-int-min-max
+/*eslint no-unused-vars: "error"*/
+// eslint-disable-next-line no-unused-vars
 function randomInteger(min, max) {
   min = Number(min);
   max = Number(max);
   if(
-    isNaN(min)
-    || isNaN(max)
-    || min < 0
-    || max < 0
-    || min >= max
+    min >= 0
+    && max >= 0
+    && min < max
   ) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  } else {
     return false;
   }
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
 }
 
-/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
-/* eslint no-console: 0 */
-console.log(randomInteger)
-
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+// eslint-disable-next-line no-unused-vars
 function randomFloat(min, max, precision = 2) {
   min = Number(min);
   max = Number(max);
   precision = parseInt(precision);
   if(
-    isNaN(min)
-    || isNaN(max)
-    || isNaN(precision)
-    || min < 0
-    || max < 0
-    || precision < 0
-    || min >= max
+    min >= 0
+    && max >= 0
+    && min < max
+    && precision >= 0
   ) {
+    let rand = min + Math.random() * (max - min);
+    let result = rand.toFixed(precision) - 0;
+    return result > max ? max : result;
+  } else {
     return false;
   }
-  let rand = min + Math.random() * (max - min);
-  let result = rand.toFixed(precision) - 0;
-  return result > max ? max : result;
 }
-
-console.log(randomFloat)
-
-// https://stackoverflow.com/questions/4187146/truncate-number-to-two-decimal-places-without-rounding
-/*
-function randomFloat(min, max, precision = 2) {
-  let rand = min + Math.random() * (max - min);
-  // .toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]
-  let reg = new RegExp('^-?\\d+(?:\\.\\d{0,' + precision + '})?');
-  let result = Math.abs(rand.toString().match(reg)[0] - 0);
-  return result < min - 0 ? min : result;
-}
-*/
