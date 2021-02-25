a154c9a521803807d5d402d926967c95b16294de
Maria Zakharenkova <67202380+kusyash@users.noreply.github.com>
2021-02-25 22:40:58 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-// eslint-disable-next-line no-unused-vars
-import { getRandomFloat, getRandomInteger } from './numbers.js';
-// eslint-disable-next-line no-unused-vars
-import { getRandomArray, getRandomArrayElement, shuffleArray } from './arrays.js';
-// eslint-disable-next-line no-unused-vars
-import { generateData } from './data.js';
 import './offer.cards.js';
